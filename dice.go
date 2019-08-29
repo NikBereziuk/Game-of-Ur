5196bce65b05d9c18017f7f3bf9316d219185ab6
Mykyta Bereziuk <nikbereziuk@gmail.com>
2019-08-29 18:02:27 +0200

    Dice logic

---
+++ dice.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+type dice [4]int
+
+func (d dice) roll() dice {
+
+	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	result := rand.Intn(9999)
+
+	for i := 0; i < len(d); i++ {
+		remaind := result % 10
+		if remaind%2 == 0 {
+			d[i] = 0
+		} else {
+			d[i] = 1
+		}
+		result = result / 10
+	}
+
+	return d
+}
